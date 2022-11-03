3a4e77709992284e0a155aad7299595951d7954b
Mateusz Olszewski <mateusz.olszewski@pictec.eu>
2022-11-03 17:18:33 +0100

    allow all to read places

--- parkings/api/places/views.py
+++ parkings/api/places/views.py
@@ -1,10 +1,11 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 from parkings.api.places.serializers import PlacesSerializer
 from parkings.models import ParkingArea
 
 
 class PlacesViewSet(viewsets.ReadOnlyModelViewSet):
+    permission_classes = [permissions.AllowAny]
     queryset = ParkingArea.objects.all()
     serializer_class = PlacesSerializer
     lookup_field = "origin_id"

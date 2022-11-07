5f09ee67479c2a0dfdf4986621ca5f32225964f5
Mateusz Olszewski <mateusz.olszewski@pictec.eu>
2022-11-07 10:20:09 +0100

    filter on modification

--- parkings/api/places/filters.py
+++ parkings/api/places/filters.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.contrib.gis.geos import Point
 from django.contrib.gis.measure import D
 
@@ -10,13 +12,15 @@ class PlacesFilter(filters.FilterSet):
     latitude = filters.NumberFilter(field_name="latitude", method="filter_location")
     longitude = filters.NumberFilter(field_name="longitude", method="filter_location")
     radius = filters.NumberFilter(field_name="radius", method="filter_location")
+    modified_since = filters.NumberFilter(field_name="modified_at", method="filter_modified_since")
     
     class Meta:
         model = ParkingArea
         fields = (
             "latitude",
             "longitude",
-            "radius"
+            "radius",
+            "modified_since"
         )
         
     def filter_location(self, queryset, name, value):
@@ -29,3 +33,6 @@ class PlacesFilter(filters.FilterSet):
                 )
             )
         return queryset
+
+    def filter_modified_since(self, queryset, name, value):
+        return queryset.filter(modified_at__gte=datetime.fromtimestamp(value))

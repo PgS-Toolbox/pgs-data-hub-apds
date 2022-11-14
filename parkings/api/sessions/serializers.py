38af7ee0ca9d97b90ae91c1cf9dc8c422c8ebf50
Mateusz Olszewski <mateusz.olszewski@pictec.eu>
2022-11-14 12:55:02 +0100

    create parking from session

--- parkings/api/sessions/serializers.py
+++ parkings/api/sessions/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from parkings.models import Parking
+from parkings.api.operator.parking import OperatorAPIParkingSerializer
 
 
 class SessionsSerializer(serializers.ModelSerializer):
@@ -20,6 +21,51 @@ class SessionsSerializer(serializers.ModelSerializer):
             "segments"
         )
 
+    def create(self, validated_data):
+        return OperatorAPIParkingSerializer(data=validated_data)
+
+    def validate(self, data):
+        registration_number = ""
+        for credentials in data["identifiedCredentials"]:
+            if credentials["type"] == "licensePlate":
+                if identifier := credentials.get("identifier"):
+                    registration_number = identifier.get("id")
+                else:
+                    raise serializers.ValidationError("No 'id' in identifiedCredentials identifier")
+            else:
+                raise serializers.ValidationError("No 'identifier' in identifiedCredentials")
+        if registration_number == "":
+            raise serializers.ValidationError("There is no 'licensePlate' in `identifiedCredentials`")
+
+        parking_zone = None
+        if type(data["segments"]) != list:
+            raise serializers.ValidationError("Wrong type for 'segments', should be a list")
+        for segment in data["segments"]:
+            if type(segment) != dict:
+                raise serializers.ValidationError("Wrong type for segment, should be a dict")
+            if assign_right := segment.get("assignedRight"):
+                if right_specification := assign_right.get("rightSpecification"):
+                    if right_specification.get("className") and right_specification.get("className") == "ParkingZone":
+                        if parking_zone is None:
+                            parking_zone = right_specification.get("id")
+                        elif parking_zone != right_specification.get("id"):
+                            raise serializers.ValidationError("Different parking zones in one session")
+
+        if parking_zone is None:
+            raise serializers.ValidationError("No information about parking zone")
+
+        parking_data = {
+            #"location": None,
+            #"terminal_number": None,
+            "registration_number": registration_number,
+            "time_start": data["actualStart"],
+            "time_end": data.get("actualEnd"),
+            "zone": parking_zone,  
+        }
+        OperatorAPIParkingSerializer.validate(parking_data)
+        return parking_data
+
+
     def get_actualStart(self, obj):
         return obj.time_start
 
@@ -36,7 +82,7 @@ class SessionsSerializer(serializers.ModelSerializer):
     def get_identifiedCredentials(self, obj):
         return [{
             "identifier": {
-                "className": "registration_number",
+                "className": "RegistrationNumber",
                 "id": obj.normalize_reg_num
             },
             "issuer": [{
@@ -57,17 +103,18 @@ class SessionsSerializer(serializers.ModelSerializer):
                 "version": 1,
                 "rightHolder": [{
                     "identifier": {
-                        "className": "registration_number",
+                        "className": "RegistrationNumber",
                         "id": obj.normalize_reg_num
                         },
                     "issuer": [{
                         "language": "en",
-                        "string": ""
+                        "string": "",
                     }]
                 }],
                 "rightSpecification": {
-                    "id": obj.id,
-                    "version": 1
+                    "id": obj.zone,
+                    "version": 1,
+                    "className": "ParkingZone"
                 },
                 "expiry": obj.time_end,
                 "issuanceTime": obj.created_at,

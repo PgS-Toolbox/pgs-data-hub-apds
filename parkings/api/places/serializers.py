0399c1954343fd1f3c8e945fea3d07a122e6feca
Mateusz Olszewski <mateusz.olszewski@pictec.eu>
2022-11-25 00:49:33 +0100

    update PlacesSerializer

--- parkings/api/places/serializers.py
+++ parkings/api/places/serializers.py
@@ -8,67 +8,25 @@ from parkings.models import ParkingArea
 
 
 class PlacesSerializer(serializers.ModelSerializer):
-    name = serializers.SerializerMethodField()
-    description = serializers.SerializerMethodField()
     layer = serializers.SerializerMethodField()
     type = serializers.SerializerMethodField()
-    aliases = serializers.SerializerMethodField()
-    parentId = serializers.SerializerMethodField()
-    childIds = serializers.SerializerMethodField()
-    operatorDefinedReference = serializers.SerializerMethodField()
-    hierarchyElementRecord = serializers.SerializerMethodField()
-    rightSpecifications = serializers.SerializerMethodField()
     hierarchyElementReference = serializers.SerializerMethodField()
-    occupancyLevel = serializers.SerializerMethodField()
     spaceBoundedZone = serializers.SerializerMethodField()
     
     class Meta:
         model = ParkingArea
         fields = (
-            "name",
-            "description",
             "layer",
             "type",
-            "aliases",
-            "parentId",
-            "childIds",
-            "operatorDefinedReference",
-            "hierarchyElementRecord",
-            "rightSpecifications",
             "hierarchyElementReference",
-            "occupancyLevel",
             "spaceBoundedZone"
         )
-
-    def get_name(self, obj):
-        return None
-
-    def get_description(self, obj):
-        return None
 
     def get_layer(self, obj):
         return 1
 
     def get_type(self, obj):
         return "place"
-
-    def get_aliases(self, obj):
-        return None
-
-    def get_parentId(self, obj):
-        return None
-
-    def get_childIds(self, obj):
-        return None
-
-    def get_operatorDefinedReference(self, obj):
-        return None
-
-    def get_hierarchyElementRecord(self, obj):
-        return None
-    
-    def get_rightSpecifications(self, obj):
-        return None
 
     def get_hierarchyElementReference(self, obj):
         now = timezone.now()
@@ -78,9 +36,9 @@ class PlacesSerializer(serializers.ModelSerializer):
         
         return {
             "elementId": {
-                "id": "string",
-                "version": None,
-                "className": None
+                "id": obj.origin_id,
+                "version": 1,
+                "className": "ParkingArea"
             },
             "demandTable": {
                 "demandType": {
@@ -93,21 +51,6 @@ class PlacesSerializer(serializers.ModelSerializer):
             "supply": {
                 "supplyViewType": "vehicleView",
                 "supplyQuantity": obj.estimated_capacity,
-            }
-        }
-
-    def get_occupancyLevel(self, obj):
-        return {
-            "occupancyIndicator": {
-                "codeListEntryId": {
-                    "className": None
-                },
-                "codeListId": {
-                    "id": None,
-                    "version": 1,
-                    "className": None
-                },
-                "entryDefinedValue": None
             }
         }
 

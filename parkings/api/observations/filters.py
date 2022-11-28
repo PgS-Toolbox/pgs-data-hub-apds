9df8b663f9241f398be33a799dfeb6af62923458
Mateusz Olszewski <mateusz.olszewski@pictec.eu>
2022-11-28 09:12:41 +0100

    correct ovservations filter

--- parkings/api/observations/filters.py
+++ parkings/api/observations/filters.py
@@ -1,3 +1,8 @@
+from datetime import datetime
+
+from django.contrib.gis.geos import Point
+from django.contrib.gis.measure import D
+
 import django_filters as filters
 
 from parkings.models import ParkingCheck
@@ -16,7 +21,7 @@ class ObservationFilter(filters.FilterSet):
             center = Point(float(params["longitude"]), float(params["latitude"]))
             return (
                 queryset.filter(
-                    geom__distance_lte=(center, D(m=float(params["radius"])))
+                    location__distance_lte=(center, D(m=float(params["radius"])))
                 )
             )
         return queryset
